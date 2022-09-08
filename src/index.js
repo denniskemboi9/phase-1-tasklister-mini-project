699fb17e4c5df9cb674d54e395b01ce7c387afeb
Dennis Kemboi <cherogonydennis9@gmail.com>
2022-09-08 09:07:24 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const form = document.getElementById("create-task-form");
+  form.addEventListener('submit', newTask);
 });
+
+function listenSubmit(event) {
+  event.preventDefault();
+  const newTask = document.getElementById("new-task-description").value;
+  const ul = document.getElementById("tasks")
+  const li = document.createElement('li');
+  li.textContent = newTask
+  li.appendChild(document.createTextNode(task));
+  li.append(deleteBtn)
+  ul.appendChild(li);
+  event.target.reset()
+};
